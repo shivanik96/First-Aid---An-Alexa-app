f71887dece5723e52765f9a1154602febc999754
Shivani Kumar <33324565+shivanik96@users.noreply.github.com>
2018-04-29 15:03:18 +0530

    Create first_aid_json.js

---
+++ first_aid_json.js
@@ -0,0 +1,236 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "first aid",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "getFirstAidIntent",
+                    "slots": [
+                        {
+                            "name": "problem",
+                            "type": "problem"
+                        }
+                    ],
+                    "samples": [
+                        "procedure {problem}",
+                        "in {problem}",
+                        "what to {problem}",
+                        "help {problem}",
+                        "help for {problem}",
+                        "first {problem}",
+                        "how to help when {problem}",
+                        "{problem}",
+                        "what to do when {problem}",
+                        "first aid for {problem}"
+                    ]
+                }
+            ],
+            "types": [
+                {
+                    "name": "problem",
+                    "values": [
+                        {
+                            "name": {
+                                "value": "vertigo"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "testicle pain"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "rectal bleeding"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "pulled muscles"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "insect bites and stings"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "hypothermia"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "heatstroke"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "heat exhaustion"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "heart palpitations"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "frostbite"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "food poisoning"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "fever in children"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "dizziness "
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "diarrhea"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "choking"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "broken toe"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "bruises"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "blood in urine"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "abdominal pain"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "strains"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "sprains"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "rash"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "sunburn"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "minor burns"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "burns"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "spider bite"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "mosquito bite"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "sting",
+                                "synonyms": [
+                                    "insect sting",
+                                    "bee sting"
+                                ]
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "animal bite"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "nosebleeds",
+                                "synonyms": [
+                                    "nosebleed"
+                                ]
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "scrape"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "minor cut"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "heart attack"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "stroke"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "bite"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "cut"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
